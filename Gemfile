d7b433233b55c327436a85402305611a5c2ebbdf
Lars Vonk <lars.vonk@gmail.com>
2015-07-02 13:36:48 +0200

    Simple setup of fitnesse with ruby-slim

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+
+gem 'rubyslim', git: 'git@github.com:lvonk/rubyslim.git'
---
+++ fixtures/slim.rb
@@ -0,0 +1,11 @@
+module Fixtures
+  class Slim
+    def initialize
+
+    end
+
+    def message
+      "it works"
+    end
+  end
+end
